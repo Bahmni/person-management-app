bc50625c9218a08638756764505b3c1c6bbf5701
Georgina Hodgkinson <Georginahodgkinson@Yoda-1.connect>
2018-07-11 16:57:21 +0100

    added validation to givenName and lastName

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const url = process.env.REACT_APP_URL
-const data = {
-  "names": [
-    {
-    "display": "Sam Smith",
-    "familyName": "Smith",
-    "givenName": "Sam",
-    "preferred": false
-    }
-  ],
-  "gender": "M",
-  "age": 0,
-  "birthdate": "1989-07-01T00:00:00.000+0200",
-  "birthdateEstimated": false,
-  "dead": false,
-  "deathDate": null,
-  "causeOfDeath": null,
-  "addresses": [
-    {}
-  ],
-  "deathdateEstimated": false,
-  "birthtime": null
-}
+// const url = "https://192.168.33.10/openmrs/ws/rest/v1/person";
+// const data = {
+//   "names": [
+//     {
+//     "display": "Sam Smith",
+//     "familyName": "Smith",
+//     "givenName": "Sam",
+//     "preferred": false
+//     }
+//   ],
+//   "gender": "M",
+//   "age": 0,
+//   "birthdate": "1989-07-01T00:00:00.000+0200",
+//   "birthdateEstimated": false,
+//   "dead": false,
+//   "deathDate": null,
+//   "causeOfDeath": null,
+//   "addresses": [
+//     {}
+//   ],
+//   "deathdateEstimated": false,
+//   "birthtime": null
+// }
 
+// fetch(url, {
+//   method: 'POST',
+//   body: JSON.stringify(data), // data can be `string` or {object}!
+//   headers:{
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+//   credentials: 'include'
 
-
-fetch(url, {
-  method: 'POST',
-  body: JSON.stringify(data), // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  },
-  credentials: 'include'
-
-}).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => console.log('Success:', response));
-
-
+// }).then(res => res.json())
+// .catch(error => console.error('Error:', error))
+// .then(response => console.log('Success:', response));
 
 class App extends Component {
 // givenName, familyName, age, gender
@@ -51,14 +47,22 @@ class App extends Component {
       // age: ,
       // gender:
     },
-    persons: [],
+    fieldErrors: {},
+    people: [],
   }
 
   onFormSubmit = (e) => {
     // create a copy using spread (keep persons immutable) + add current input value
-    const persons = [...this.state.persons, this.state.fields];
+    const people = [...this.state.people ];
+    const person = this.state.fields;
+    const fieldErrors = this.validate(person);
+      this.setState({fieldErrors});
+      e.preventDefault();
+
+      if (Object.keys(fieldErrors).length) return;
+      
     this.setState({
-      persons,
+      people: people.concat(person),
       fields: {
         // reset fields back to empty after submit
         givenName: '',
@@ -69,7 +73,14 @@ class App extends Component {
     e.preventDefault();
   };
 
-  onInputChange = (e) => {
+  validate = (person) => {
+    const errors = {};
+    if (!person.givenName) errors.givenName = 'Given Name Required';
+    if (!person.familyName) errors.familyName = 'Family Name Required';
+    return errors;
+  };
+
+    onInputChange = (e) => {
     const fields = this.state.fields;
     fields[e.target.name] = e.target.value;
     this.setState({ fields });
@@ -87,6 +98,7 @@ class App extends Component {
             value={this.state.fields.givenName}
             onChange={this.onInputChange}
           />
+          <span style={{ color: 'red' }}>{ this.state.fieldErrors.givenName }</span>
           {/* family name */}
           <input
             placeholder='Family name'
@@ -94,6 +106,7 @@ class App extends Component {
             value={this.state.fields.familyName}
             onChange={this.onInputChange}
           />
+          <span style={{ color: 'red' }}>{ this.state.fieldErrors.familyName }</span>
           {/* submit */}
           <input type="submit"/>
         </form>
@@ -101,7 +114,7 @@ class App extends Component {
           <h3>Submitted Persons</h3>
           <ul>
             {/* we use i to set a unique key id for each li element */}
-            { this.state.persons.map(({givenName, familyName}, i) =>
+            { this.state.people.map(({givenName, familyName}, i) =>
               <li key={i}>{givenName} {familyName}</li>
             )}
           </ul>
