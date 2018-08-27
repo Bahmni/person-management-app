dd7c27dcaf807652dce77c88e8bf4fd121e55d17
EVA <edie.blues@gmail.com>
2018-08-27 08:02:12 +0100

    new modal for SVG, deleted modalError modalSuccess css files

--- src/components/modals/ModalSuccess.js
+++ src/components/modals/ModalSuccess.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './ModalSuccess.css';
 import './Modal.css';
+import Icon from './Icon';
 
 class ModalSuccess extends Component {
   // set timeout, default setting is 4 seconds
   timeout = 4000;
   componentDidMount() {
-    this.timer = setTimeout(() => {
+    setTimeout(() => {
       this.props.onClose();
     }, this.timeout);
   }
 
-  componentWillUnMount() {
-    clearTimeout(this.timer);
-  }
-
   handleClick(e) {
-    this.props.onClose(e);
-    clearTimeout(this.timer);
-  }
-
-  handleOutsideClick(e) {
     if (!this.node.contains(e.target)) {
-      this.handleClick(e);
+      this.props.onClose(e);
     }
   }
   render() {
+    const modalBackgroundColor = {
+      backgroundColor: 'rgba(15, 113, 101, 0.65)'
+    };
     return (
-      <div className="backdrop" onClick={e => this.handleOutsideClick(e)}>
-        <div className="modal modalSuccess" ref={node => (this.node = node)}>
-          <div className="modalLeft modalLeftSuccess" />
+      <div className="backdrop" onClick={e => this.handleClick(e)}>
+        <div
+          className="modal"
+          style={modalBackgroundColor}
+          ref={node => (this.node = node)}
+        >
+          <Icon icon="success" />
           <div className="modalRight">
             <p>{this.props.lastCreatedPerson}</p>
             {this.props.text.map((t, index) => <p key={index}>{t}</p>)}
