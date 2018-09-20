58929259948761fbca8f356f94ebeaee4aecf5a5
Georgina Hodgkinson <Georginahodgkinson@yoda-1.home>
2018-09-20 11:53:49 +0100

    number of persons

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -4,28 +4,31 @@ const Table = props => {
   const { data } = props;
 
   return (
-    <table className="resultsTable">
-      <thead>
-        <tr className="resultsTableHeader">
-          <th>NAME</th>
-          <th>GENDER</th>
-          <th>AGE</th>
-          <th>REGISTRATION</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(function(item, key) {
-          return (
-            <tr key={key} className="resultsTableBody">
-              <td>{item.display}</td>
-              <td>{item.gender[0]}</td>
-              <td>{item.age}</td>
-              <td>{item.dateCreated.substring(0, 10)}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <p>{data.length} Persons found </p>
+      <table className="resultsTable">
+        <thead>
+          <tr className="resultsTableHeader">
+            <th>NAME</th>
+            <th>GENDER</th>
+            <th>AGE</th>
+            <th>REGISTRATION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(function(item, key) {
+            return (
+              <tr key={key} className="resultsTableBody">
+                <td>{item.display}</td>
+                <td>{item.gender}</td>
+                <td>{item.age}</td>
+                <td>{item.dateCreated.substring(0, 10)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
