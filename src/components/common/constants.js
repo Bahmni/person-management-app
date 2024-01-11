68bb2ae58f191fc7237a5370134788f63ec52efb
Arjun-Go <arjun.g@thoughtworks.com>
2024-01-11 20:17:12 +0530

    update. Other information to handle configurable fields

--- src/components/common/constants.js
+++ src/components/common/constants.js
@@ -2,19 +2,12 @@ const hostUrl = localStorage.getItem('host')
   ? 'https://' + localStorage.getItem('host')
   : '';
 const RESTWS_V1 = hostUrl + '/openmrs/ws/rest/v1';
+const bahmniConfig = hostUrl + '/bahmni_config';
 export const Constants = {
   person: RESTWS_V1 + '/person',
-  personAttributeType: RESTWS_V1 + '/personattributetype'
+  personAttributeType: RESTWS_V1 + '/personattributetype',
+  registrationConfig: bahmniConfig + '/openmrs/apps/registration/app.json'
 };
 export const genderOptions = ['', 'Male', 'Female', 'Other'];
-export const personAttributes = {
-  organization: 'organization',
-  email: 'email',
-  mobilePhone: 'mobilePhone',
-  workPhone: 'workPhone',
-  residencePhone: 'residencePhone',
-  otherPhone: 'otherPhone',
-  occupation: 'occupationNew'
-};
 export const phoneNumberPattern = '[0-9]{10}';
 export const emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
